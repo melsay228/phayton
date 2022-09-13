e3408ac197d5cfb642076cd8b3b11d3e77244109
melsay228 <113444711+melsay228@users.noreply.github.com>
2022-09-13 17:11:30 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+user_kilogram = float(input("Сколько у вас килограмм?"))
+kilogram = 1
+pounds = 2.2
+print("У вас", round (user_kilogram / kilogram, 2), "килограмм")
+print("У вас", round (user_kilogram / pounds, 2), "фунтов")
